3716885e12588c02a0f60f5623ac0057639b0bd6
guohai-sys <60678218+guohai-sys@users.noreply.github.com>
2020-02-07 14:59:06 +0800

    Update Test.java

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -3,6 +3,6 @@ package test;
 public class Test {
 	public static void main(String[] args) {
 		System.out.println(564544554);
-		System.out.println(121213131);
+		//System.out.println(121213131);
 	}
 }
