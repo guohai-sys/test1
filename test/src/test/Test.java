fd051875c2eea47344324553a1d95ccc00b0d285
11548 <11548@DESKTOP-TRRE2AI>
2020-02-07 17:12:11 +0800

    11

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -4,7 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		//我是第一个修改的
 		System.out.println("我是第一个修改的");
-		
 		System.out.println("我是第主分支修改的");
 		System.out.println("我是第二个分支修改的");
 		//这下有版本冲突了
