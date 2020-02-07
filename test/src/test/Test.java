6cc47e0c80558c4dbbfa0f724f91da1a3043ec49
11548 <11548@DESKTOP-TRRE2AI>
2020-02-07 15:01:04 +0800

    1

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -2,8 +2,7 @@ package test;
 
 public class Test {
 	public static void main(String[] args) {
-		System.out.println(564544554);
-		System.out.println(121213131);
-		System.out.println(5);
+
+
 	}
 }
