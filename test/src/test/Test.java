41bce93c72598093f8f6cd8504d178c68846404c
11548 <11548@DESKTOP-TRRE2AI>
2020-02-07 16:47:43 +0800

    1

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -3,7 +3,7 @@ package test;
 public class Test {
 	public static void main(String[] args) {
 		//我是第一个修改的
-		System.out.println("我是第二个修改的");
+		System.out.println("我是第一个修改的");
 		System.out.println("我是第主分支修改的");
 	}
 }
