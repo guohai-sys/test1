1e7631f5dd2ef7d37bca30ad89867e22b8e03623
11548 <11548@DESKTOP-TRRE2AI>
2020-02-07 16:31:14 +0800

    1

--- test/src/test/Test.java
+++ test/src/test/Test.java
@@ -4,6 +4,6 @@ public class Test {
 	public static void main(String[] args) {
 		//我是第一个修改的
 		System.out.println("我是第二个修改的");
-		System.out.println("我是第二个分支修改的");
+		System.out.println("我是主分支修改的");
 	}
 }
--- test/src/test/Test3.java
+++
@@ -1,8 +0,0 @@
-package test;
-
-public class Test3 {
-	public static void main(String[] args) {
-		System.out.println("我是一个分支s");
-		System.out.println("先提交到本地仓库，在push到github仓库");
-	}
-}
